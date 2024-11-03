da2527b91ae1f3ad2895051446fef99ead655cd6
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2024-11-03 23:11:20 +0800

    Update vite.config.js

--- login-frontend/vite.config.js
+++ login-frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Uncomment this block if you need to externalize axios or other modules
+  // build: {
+  //   rollupOptions: {
+  //     external: ['axios'], // Specify external modules here
+  //   },
+  // },
 });
