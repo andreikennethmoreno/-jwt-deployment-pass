855f38240a5a1574087b3ac85630ae2474242a2e
andreikennethmoreno <124364969+andreikennethmoreno@users.noreply.github.com>
2024-11-03 23:07:41 +0800

    Update Users.jsx

--- login-frontend/src/components/Users.jsx
+++ login-frontend/src/components/Users.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from './axios';
 
 export default function Users() {
     const [users, setUsers] = useState([]); // State to hold user names
